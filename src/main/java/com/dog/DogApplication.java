82972e416ed6318eda94a14a2ca17a955e71d1f7
linkwang <linkwang@jidouauto.com>
2020-09-29 18:59:13 +0800

    初始化项目

---
+++ src/main/java/com/dog/DogApplication.java
@@ -0,0 +1,22 @@
+package com.dog;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping
+@SpringBootApplication
+public class DogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DogApplication.class);
+    }
+
+    @ResponseBody
+    @GetMapping("/ok")
+    public String ok(){
+        return "ok";
+    }
+}
